4a6da6fe76ed6439ee51fefb8920c7c14b8c247b
JGrospe2425 <grospe.jc@obmontessori.edu.ph>
2025-09-22 13:22:35 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+from pyscript import display
+
+# Header
+contact = '📩 Contact Us'
+note = "We’d love to hear from you!"
+
+# Location
+location = "📍 Our Location"
+street = "456 Food Street"
+city = "Golden City, Manila"
+country = "Philippines"
+
+# Phone
+phone = "📞 Phone Numbers"
+main = "Main: (02) 123-4567"
+mobile = "Mobile: 0917-654-3210"
+
+# Email
+email = "✉️ Email Addresses"
+info = "info@goldenspoon.com"
+support = "support@goldenspoon.com"
+careers = "careers@goldenspoon.com"
+
+# Footer
+footer = '⏰ Open Daily: 10:00 AM – 10:00 PM'
+
+# Display Header
+display(f"{contact}", target="Contact")
+display(f"{note}", target="Note")
+
+# Display Location
+display(f"{location}", target="Location")
+display(f"{street}", target="Street")
+display(f"{city}", target="City")
+display(f"{country}", target="Country")
+
+# Display Phone
+display(f"{phone}", target="Phone")
+display(f"{main}", target="Main")
+display(f"{mobile}", target="Mobile")
+
+# Display Email
+display(f"{email}", target="Email")
+display(f"{info}", target="Info")
+display(f"{support}", target="Support")
+display(f"{careers}", target="Careers")
+
+# Display Footer
+display(f"{footer}", target="Footer")
